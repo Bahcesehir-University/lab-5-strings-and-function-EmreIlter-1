08d04d5b7226df5eb472b05235ec4353d90cf2ec
EmreIlter <mehmetemre.ilter@bahcesehir.edu.tr>
2026-04-07 13:30:29 +0300

    Update MainProgram.cpp

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -151,9 +151,9 @@ MyString MyString::toLowerCase() const {
 }
 
 MyString MyString::trim() const {
-    size_t start = data.find_first_not_of("\t\n\r");
+    size_t start = data.find_first_not_of(" \t\n\r");
     if(start == string::npos) return MyString("");
-    size_t end = data.find_last_not_of("\t\n\r");
+    size_t end = data.find_last_not_of(" \t\n\r");
     return MyString(data.substr(start, end - start + 1));
     // TODO: Return a NEW MyString with leading and trailing whitespace removed
     // Whitespace includes: space, tab (\t), newline (\n), carriage return (\r)
